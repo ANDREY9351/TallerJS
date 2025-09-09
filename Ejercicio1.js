167c6823a73119574d4a2f42e666c7d473e0bbb8
ANDREY9351 <franyinandreyparragiraldo@gmail.com>
2025-09-09 16:32:12 -0500

    Agrego los 20 ejercicios del taller en JS

---
+++ Ejercicio1.js
@@ -0,0 +1,7 @@
+// Nombre completo: Franyin Andrey Parra Giraldo
+// Redondear un número a 2 decimales usando Math.round. Ejemplo: 3.14159
+// → 3.14.
+
+let numero = 3.14159;
+let redondeado = Math.round(numero * 100) / 100;
+console.log(redondeado);
